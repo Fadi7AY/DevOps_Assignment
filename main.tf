fa0ad129a6dbc60cf8d712a66c7888426dc1ecad
Fadi7AY <fadeyaseen3@gmail.com>
2025-07-09 01:17:19 +0300

    added prevent_destroy so the subscribtion is not removed with each terraform destroy

--- main.tf
+++ main.tf
@@ -114,7 +114,22 @@ resource "aws_sns_topic_subscription" "user_updates" {
   topic_arn = aws_sns_topic.assign_topic.arn
   protocol = "email"
   endpoint  = "fadeyaseen3@gmail.com" #this is for testing 
+
+  lifecycle {  # added this so the subscribtion stays and is not removed
+    prevent_destroy = true
+  }
 }
+
+# resource "aws_sns_topic_subscription" "user_updates_2" {    THIS WAS USED TO TEST THE prevent_destroy
+#   topic_arn = aws_sns_topic.assign_topic.arn
+#   protocol = "email"
+#   endpoint  = "fadeyaseen6@gmail.com" #this is for testing 
+
+#   lifecycle {  # added this so the subscribtion stays and is not removed
+#     prevent_destroy = true
+#   }
+# }
+
 
 
 
