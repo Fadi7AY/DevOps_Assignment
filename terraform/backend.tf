18bf4f9a4996e87c79d225816726e2f38f80e104
Fadi7AY <fadeyaseen3@gmail.com>
2025-07-13 14:17:56 +0300

    changed state bucket name to test bucket lost scenario

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-fadi7ay"
+    bucket  = "terraform-state-fadi7ay-testing2"
     key     = "devops-assignment/terraform.tfstate"
     region  = "eu-central-1"
     encrypt = true
